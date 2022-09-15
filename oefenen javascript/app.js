ba410eb3e6d4a17692e21ecaa6e03627415f9b95
MarkvanderGiessen <9020308@student.zadkine.nl>
2022-09-15 14:59:41 +0200

    added new things

--- oefenen javascript/app.js
+++ oefenen javascript/app.js
@@ -8,8 +8,17 @@ const myName = "mark";
 let myAge = 17;
 const diploma = "true";
 let passed;
-
+let num1 = 69;
+let num2 = 13;
+let answer = num1 + num2; 
+let answer = num1 - num2;
+let answer = num1 * num2;
+let answer = num1 / num2;
 document.write(myName)
 document.write(myAge)
 document.write(diploma)
 document.write(passed)
+console.log(answer1)
+console.log(answer2)
+console.log(answer3)
+console.log(answer4)
