e9f9b2292444da7bbe6370dadc41124f7b485c08
MarkvanderGiessen <9020308@student.zadkine.nl>
2022-09-15 14:40:43 +0200

    verandering javascript

---
+++ oefenen javascript/app.js
@@ -0,0 +1,15 @@
+
+let name = "rouhi";
+const animal = "shark";
+alert = "animal"
+let num = 32
+let text = "ik ben een tekst"
+const myName = "mark";
+let myAge = 17;
+const diploma = "true";
+let passed;
+
+document.write(myName)
+document.write(myAge)
+document.write(diploma)
+document.write(passed)
