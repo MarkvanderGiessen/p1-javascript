f673ff2b689a217422c4409ec60acbf1c6cac479
MarkvanderGiessen <9020308@student.zadkine.nl>
2022-10-06 14:58:24 +0200

    opdracht 6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,17 @@
+function calculate(num= 7) {
+    num+=5;
+    console.log(num);
+}
+calculate();
+
+function multiplybyfive(num2= 8) {
+    num2*=5;
+    console.log(num2);
+}
+multiplybyfive();
+
+function calculateMinutes(num3= 3600){
+      num3/=60;
+      console.log(num3)
+}
+    calculateMinutes()
