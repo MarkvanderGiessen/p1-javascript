14c7ec19f50bd63bb043abea7ac2dadbfc132778
MarkvanderGiessen <9020308@student.zadkine.nl>
2022-11-10 14:16:38 +0100

    opdracht_9.1

---
+++ opdracht_9.1/app.js
@@ -0,0 +1,24 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function verification() {
+  let age = document.getElementById("age");
+  if (age.value > 18) {
+    window.location.href = "/opdracht_9.1/oldenough.html";
+  } else {
+    window.location.href = "/opdracht_9.1/toYoung.html";
+    console.log("niet oud genoeg");
+  }
+}
